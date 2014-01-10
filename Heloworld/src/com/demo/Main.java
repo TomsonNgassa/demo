189f1464db708ef08652964a5e20aa04249ec781
tomson.ngassa <tomson.ngassa@TomsonNgassaPC.fei.local>
2014-01-10 16:01:02 -0500

    Change print message

--- Heloworld/src/com/demo/Main.java
+++ Heloworld/src/com/demo/Main.java
@@ -3,7 +3,7 @@ package com.demo;
 public class Main {
 
 	public static void main(String[] args) {
-		System.out.println("Hello my name is Tomson.");
+		System.out.println("My name is Tomson.");
 	}
 
 }
